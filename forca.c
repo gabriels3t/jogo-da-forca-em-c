57f9da5dc23b8727f7b4257c950124a9f647ccd1
gabriels3t <gabriels3t@hotmail.com>
2021-05-25 19:07:00 -0300

    Leitura e escrita em txt

--- forca.c
+++ forca.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 
+//VARIAVEIS GLOBAIS
 char palavraSecreta[20];
 char chutes[26];
 int tentativas = 0;
+
+//MOSTRANDO AS FUNÇÕES QUE EXISTE PRO COMPILADOR NÃO DAR ERRO DE ORDEM
+void abertura();
+void escolhePalavra();
+void chuta();
+int jaChutou(char letra);
+void desenhaForca();
+int acertou();
+int inforcou();
+
 void abertura(){
     printf("/****************/\n");
     printf("/ Jogo de Forca */\n");
@@ -50,14 +61,38 @@ void desenhaForca(){
         printf("\n");
 }
 
+int acertou(){
+    for(int j=0;j<strlen(palavraSecreta);j++){
+        if(!jaChutou(palavraSecreta[j])){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int inforcou(){
+    int erro = 0; 
+    for(int i =0; i<tentativas;i++){
+        int existe = 0;
+        for(int j=0;j<strlen(palavraSecreta);j++){
+            if(chutes[i]== palavraSecreta[j]){
+                existe =1;
+                break;
+            }
+        }
+        if(!existe) erro++;
+    }
+    return erro>=5;
+
+    
+}
+
 int main(){
-    int acertou = 0;
-    int inforcou = 0;
     escolhePalavra();
     abertura();
     do{
         desenhaForca();    
         chuta();
         
-    }while(!acertou && !inforcou);
+    }while(!acertou() && !inforcou());
 }
